c7e0b1488250008dbf26c7aa7b97ec6df1e07a48
locne <nakrothnguyen127@gmail.com>
2025-09-23 21:26:35 +0700

    Implement AuthContext with logout and refreshAuth

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import React, { createContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+interface AuthContextType {
+  user: User | null;
+  isLoading: boolean;
+  refreshAuth: () => Promise<void>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  const refreshAuth = async () => {
+    try {
+      let response = await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/v1/auth/me`, {
+        credentials: 'include'
+      });
+
+      if (response.status === 401) {
+        await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/v1/auth/refresh_token`, {
+          method: 'POST',
+          credentials: 'include'
+        });
+        response = await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/v1/auth/me`, {
+          credentials: 'include'
+        });
+      }
+
+      const data = await response.json();
+
+      setUser(data.data);
+
+    } catch (error) {
+      setUser(null);
+      console.log("error auth: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/v1/auth/logout`, {
+        method: 'POST',
+        credentials: 'include'
+      });
+
+      setUser(null);
+      router.replace('/login');
+
+    } catch (error) {
+      console.error("Logout error:", error);
+      setUser(null);
+      router.replace('/login');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    refreshAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading, refreshAuth, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
